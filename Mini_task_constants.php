3ce0f1c811115803d8c7e3333c8a63498644ffd5
Michael Hegarty <32098808+MylesHegarty-M-BPC-Magenta@users.noreply.github.com>
2017-10-17 14:17:14 +0100

    Update Mini_task_constants.php

--- Mini_task_constants.php
+++ Mini_task_constants.php
@@ -5,17 +5,17 @@ define("LastName","Myles Hegarty");
 define("FavSub","Game Development");
 
 echo FristName LastName;
-?>
 
-<?php
+
+
 $Age = 17;
 
 echo "My age add five years is :";
 echo $Age + "5"; 
-?>
 
 
-<?php
+
+
 	$browser = $_SERVER["HTTP_USER_AGENT"];
 	echo "Your browser is : $browser";
 	
