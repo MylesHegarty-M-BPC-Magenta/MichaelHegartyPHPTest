1f138bc17af71129739fb984f5e849d702a1d4d4
Michael Hegarty <32098808+MylesHegarty-M-BPC-Magenta@users.noreply.github.com>
2017-10-17 13:56:54 +0100

    Add files via upload

---
+++ Mini_task_constants.php
@@ -0,0 +1,26 @@
+
+
+<?php
+define("FirstName"," Michael");
+define("LastName","Myles Hegarty");
+define("FavSub","Game Development");
+
+echo FristName LastName;
+?>
+
+<?php
+$Age = 17;
+
+echo "My age add five years is :";
+echo $Age + "5"; 
+
+
+
+
+
+
+<?php
+	$browser = $_SERVER["HTTP_USER_AGENT"];
+	echo "Your browser is : $browser";
+	
+?>
